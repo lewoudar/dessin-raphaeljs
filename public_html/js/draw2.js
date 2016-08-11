973e6806aec6a5b603743e215f701fb117653e4a
lewoudar <lewoudar@gmail.com>
2016-08-11 16:13:22 +0200

    ajout du fichier drag-and-drop.html, mis à jour de draw2.js avec deux cercles qui bougent et une ligne

--- public_html/js/draw2.js
+++ public_html/js/draw2.js
@@ -1,7 +1,26 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var paper_width = 1000;
+var paper_heigth = 1000;
+var paper = new Raphael(0, 0, paper_width, paper_heigth);
+var radius = 25;
+var circle1 = paper.circle(120, 110, radius).attr("fill", "yellow");
+var circle2 = paper.circle(500, 310, radius).attr("fill", "yellow");
+var line = paper.path("M120,110L500,310");
+console.log(line);
 
+circle1.drag(dragmove, dragstart, dragend);
+circle2.drag(dragmove, dragstart, dragend);
 
+function dragmove(dx, dy, x, y, e) {
+       this.attr({
+        cx: x,
+        cy: y
+    }); 
+}
+
+function dragstart(x, y, e) {
+    this.attr("fill", "orange");
+}
+
+function dragend(e) {
+    this.attr("fill", "yellow");
+}
