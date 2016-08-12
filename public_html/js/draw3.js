42f5a202884f3090a3e2bed4722c1bffb34a64e0
lewoudar <lewoudar@gmail.com>
2016-08-12 15:52:58 +0200

    ajout d'instructions pour dessiner les liens(lignes) et les textes en bas des sondes(cercles)

--- public_html/js/draw3.js
+++ public_html/js/draw3.js
@@ -30,42 +30,70 @@ var radius = 25;
 //Objet que l'application devra me retourner
 var result = {
     '54': {
-        links: [55, 56]
+        links: [55, 56],
+        equipement: "sonde-54",
+        pop: "pop",
+        dsp: ""
     },
     '55': {
-        links: [54, 57]
+        links: [54, 57],
+        equipement: "sonde-55",
+        pop: "pop",
+        dsp: ""
     },
     '56': {
-        links: [54]
+        links: [54],
+        equipement: "sonde-56",
+        pop: "pop",
+        dsp: ""
     },
     '57': {
-        links: [55]
+        links: [55],
+        equipement: "sonde-57",
+        pop: "pop",
+        dsp: ""
     }
 };
 
 //Tableau des cercles
 var probes = [];
+//Tableau des noms
+var texts = [];
 
 //Premier parcours pour dessiner les cercles
 var i = 0;
 for(var id in result){
     var positionX = getRandomArbitrary(100, paper_width - 100);
     var positionY = getRandomArbitrary(100, paper_height - 100);
+    //cercle
     var circle = paper.circle(positionX, positionY, radius).attr({fill: 'yellow'});
     circle.data("id", id);
+    circle.data("links", []);
+    //texte
+    var text = paper.text(positionX, positionY + radius + 15, result[id].equipement + "\n" + result[id].pop);
+    text.attr({'font-size': '13px'});
+    //On ajoute dans les tableaux
+    texts.push(text);
     probes.push(circle);
     i++;
 }
 
-//Tableau qui doit contenir les liens dessinés
-var links = [];
+//Objet qui va contenir les liens dessinés
+//ex{"54-55":objet link}
+var links = {};
 //Second parcours pour dessiner les liens
 i = 0;
-//for(var id in result){
-//    result[id].links.forEach(function(element){
-//        if(!findLink(id, element, links)){
-//            var string_line = "M"+probes[i].attr("cx")+","+probes[i].attr("cy")+"L"
-//        }
-//    });
-//    i++;
-//}
+for(var id in result){
+    result[id].links.forEach(function(element){
+        if(findLink(id, element, Object.keys(links))){
+            probes[i].data("links").push([element]);
+        }
+        else{
+            var position = getPosition(element);
+            var string_line = "M"+probes[i].attr("cx")+","+probes[i].attr("cy")+"L"+position.x+","+position.y;
+            var line = paper.path(string_line);
+            probes[i].data("links").push([element, line]);
+        }
+    });
+    i++;
+}
