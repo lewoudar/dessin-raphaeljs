3f97000be9827d20da5cbd6a4b61914801f6a6bb
lewoudar <lewoudar@gmail.com>
2016-08-17 12:37:40 +0200

    draw3.js: j'ai créé une fonction draw_graph qui englobe une grande partie du code de dessin du graphe

--- public_html/js/draw3.js
+++ public_html/js/draw3.js
@@ -1,29 +1,29 @@
 //Fonctions utiles
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
 }
 
-function findLink(id1, id2, links){
+function findLink(id1, id2, links) {
     var find = false;
     var string_id2 = "" + id2;
-    links.forEach(function(string){
-        if(string.indexOf(id1) !== -1 && string.indexOf(string_id2) !== -1){
+    links.forEach(function (string) {
+        if (string.indexOf(id1) !== -1 && string.indexOf(string_id2) !== -1) {
             find = true;
         }
     });
     return find;
 }
 
-function getLink(id1, id2, links){
+function getLink(id1, id2, links) {
     id2 = "" + id2;
     var key = id2 + "-" + id1;
     return links[key];
 }
 
-function getPosition(id){
+function getPosition(id) {
     id = "" + id;
-    for(var j = 0; j < probes.length; j++){
-        if(probes[j].data("id") === id){
+    for (var j = 0; j < probes.length; j++) {
+        if (probes[j].data("id") === id) {
             return {x: probes[j].attr("cx"), y: probes[j].attr("cy")};
         }
     }
@@ -47,7 +47,7 @@ function dragend(e) {
 
 function dragmove(dx, dy, x, y, e) {
     this.attr({
-        cx : x,
+        cx: x,
         cy: y
     });
     //Gestion des textes
@@ -57,17 +57,16 @@ function dragmove(dx, dy, x, y, e) {
         y: this.attr("cy") + radius + text_offset
     });
     //Gestion des liens
-    this.data("links").forEach(function(element){
+    this.data("links").forEach(function (element) {
         //console.log(element);
         var path = element[2].attr("path");
         //console.log(path);
         //Si le cercle est impliqué sur le début de la ligne
-        if(element[1] === "start"){
+        if (element[1] === "start") {
             //path[0] contient un tableau de ce style [M, x, y]
             path[0][1] = x;
             path[0][2] = y;
-        }
-        else{
+        } else {
             //path[1] contient un tableau de ce style [L, x, y]
             path[1][1] = x;
             path[1][2] = y;
@@ -77,29 +76,25 @@ function dragmove(dx, dy, x, y, e) {
 }
 
 //==HOVER==
-function enterLink(){
+function enterLink() {
     this.attr({
         stroke: "blue",
         "stroke-width": "3px"
     });
 }
 
-function exitLink(){
+function exitLink() {
     this.attr({
         stroke: "black"
     });
 }
 
-var paper_width = 1000;
-var paper_height = 1000;
-var paper = new Raphael(0, 0, paper_width, paper_height);
-var radius = 25;
-var text_offset = 15;
+
 //Objet que l'application devra me retourner
 var result = {
     '54': {
         links: [55, 56],
-        id_links: {55:2, 56:1},
+        id_links: {55: 2, 56: 1},
         equipement: "sonde-54",
         pop: "pop",
         dsp: "",
@@ -111,7 +106,7 @@ var result = {
     },
     '55': {
         links: [54, 57],
-        id_links: {54:2, 57:3},
+        id_links: {54: 2, 57: 3},
         equipement: "sonde-55",
         pop: "pop",
         dsp: "",
@@ -123,7 +118,7 @@ var result = {
     },
     '56': {
         links: [54],
-        id_links: {54:1},
+        id_links: {54: 1},
         equipement: "sonde-56",
         pop: "pop",
         dsp: "",
@@ -135,7 +130,7 @@ var result = {
     },
     '57': {
         links: [55],
-        id_links: {55:3},
+        id_links: {55: 3},
         equipement: "sonde-57",
         pop: "pop",
         dsp: "",
@@ -147,88 +142,99 @@ var result = {
     }
 };
 
+var paper_width = 1000;
+var paper_height = 1000;
+var paper = new Raphael(0, 0, paper_width, paper_height);
+var radius = 25;
+var text_offset = 15;
 //Tableau des cercles
 var probes = [];
 //Tableau des noms
 var texts = [];
-
-//Premier parcours pour dessiner les cercles
-var i = 0;
-for(var id in result){
-    var positionX = getRandomArbitrary(100, paper_width - 100);
-    var positionY = getRandomArbitrary(100, paper_height - 100);
-    //cercle
-    var circle = paper.circle(positionX, positionY, radius);
-    var infos = "Température: " + result[id].temperature + "\nMise en service: "
-                + result[id].uptime + "\nUtilisation disque: " + result[id].disk_usage
-                + "\nAdresse Mac: " + result[id].mac + "\nEtat: " + result[id].state;
-    //On choisit la couleur de la sonde en fonction de certaines de ses informations
-    var fill = ""; //Couleur de la sonde
-    if(result[id].state === "on service"){
-        fill = "blue";
-    }
-    else{
-        fill = "yellow";
-    }
-    circle.attr({
-        fill: fill,
-        title: infos
-    });
-    circle.data({
-        id: id,
-        links: [],
-        index: i
-    });
-    //texte
-    var text = paper.text(positionX, positionY + radius + text_offset, result[id].equipement + "\n" + result[id].pop);
-    text.attr({'font-size': '13px'});
-    //On ajoute dans les tableaux
-    texts.push(text);
-    probes.push(circle);
-    i++;
-}
-
 //Objet qui va contenir les liens dessinés
 //ex{"54-55":objet link}
 var links = {};
+
+function draw_graph(result) {
+
+//Premier parcours pour dessiner les cercles
+    var i = 0;
+    for (var id in result) {
+        var positionX = getRandomArbitrary(100, paper_width - 100);
+        var positionY = getRandomArbitrary(100, paper_height - 100);
+        //cercle
+        var circle = paper.circle(positionX, positionY, radius);
+        var infos = "Température: " + result[id].temperature + "\nMise en service: "
+                + result[id].uptime + "\nUtilisation disque: " + result[id].disk_usage
+                + "\nAdresse Mac: " + result[id].mac + "\nEtat: " + result[id].state;
+        //On choisit la couleur de la sonde en fonction de certaines de ses informations
+        var fill = ""; //Couleur de la sonde
+        if (result[id].state === "on service") {
+            fill = "blue";
+        } else {
+            fill = "yellow";
+        }
+        circle.attr({
+            fill: fill,
+            title: infos
+        });
+        circle.data({
+            id: id,
+            links: [],
+            index: i
+        });
+        //texte
+        var text = paper.text(positionX, positionY + radius + text_offset, result[id].equipement + "\n" + result[id].pop);
+        text.attr({'font-size': '13px'});
+        //On ajoute dans les tableaux
+        texts.push(text);
+        probes.push(circle);
+        i++;
+    }
+
+
 //Second parcours pour dessiner les liens
-i = 0;
-for(var id in result){
-    result[id].links.forEach(function(element){
-        var link = null;
-        //Si le lien a déjà été dessiné, on enregistre le cercle comme
-        //étant la fin de la ligne
-        if(findLink(id, element, Object.keys(links))){
-            link = getLink(id, element, links);
-            probes[i].data("links").push([element, "end", link]);
-        }
-        //Sinon on crée le lien, on l'enregistre dans le tableau des liens et on
-        //enregistre le cercle comme étant le début de la ligne
-        else{
-            var position = getPosition(element);
-            var string_line = "M"+probes[i].attr("cx")+","+probes[i].attr("cy")+"L"+position.x+","+position.y;
-            var link = paper.path(string_line);
-            var id_link = result[id].id_links[element];
-            
-            link.attr({
-                "stroke-width": "3px",
-                title: id_link
-            });
-            probes[i].data("links").push([element, "start", link]);
-            //On enregistre le lien
-            var link_string = id + "-" + element;
-            links[link_string] = link;
-        }
-    });
-    i++;
-}
+    i = 0;
+    for (var id in result) {
+        result[id].links.forEach(function (element) {
+            var link = null;
+            //Si le lien a déjà été dessiné, on enregistre le cercle comme
+            //étant la fin de la ligne
+            if (findLink(id, element, Object.keys(links))) {
+                link = getLink(id, element, links);
+                probes[i].data("links").push([element, "end", link]);
+            }
+            //Sinon on crée le lien, on l'enregistre dans le tableau des liens et on
+            //enregistre le cercle comme étant le début de la ligne
+            else {
+                var position = getPosition(element);
+                var string_line = "M" + probes[i].attr("cx") + "," + probes[i].attr("cy") + "L" + position.x + "," + position.y;
+                var link = paper.path(string_line);
+                var id_link = result[id].id_links[element];
+
+                link.attr({
+                    "stroke-width": "3px",
+                    title: id_link
+                });
+                probes[i].data("links").push([element, "start", link]);
+                //On enregistre le lien
+                var link_string = id + "-" + element;
+                links[link_string] = link;
+            }
+        });
+        i++;
+    }
 
 //On ajoute les évènements aux éléments
 //Pour les sondes
-probes.forEach(function(element){
-    element.drag(dragmove, dragstart, dragend);
-});
+    probes.forEach(function (element) {
+        element.drag(dragmove, dragstart, dragend);
+    });
 //Pour les liens
-for(var key in links){
-    links[key].hover(enterLink, exitLink);
+    for (var key in links) {
+        links[key].hover(enterLink, exitLink);
+    }
 }
+
+//On appelle la fonction de dessin
+draw_graph(result);
